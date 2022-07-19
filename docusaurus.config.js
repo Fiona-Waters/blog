8ffdddf13b08482dc7ca5d6e63653fe9678feb2e
Fiona Waters <fiwaters6@gmail.com>
2022-07-19 11:52:00 +0100

    update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Welcome',
+  title: 'Welcome to my blog',
   tagline: 'Welcome to my blog...',
   url: 'https://fiona-waters-blog.netlify.app/',
   baseUrl: '/',
